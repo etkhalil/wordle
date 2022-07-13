1796a032262f39c102d3165a081cad7b57dd1c6d
etkhalil <coderkhalil@gmail.com>
2022-07-12 23:55:51 -0400

    add modal component

---
+++ src/components/modal.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './modal.css';
+import Button from "./button";
+
+function Modal ({handleStart, isOpen}) {
+    let classes = "modal"
+    classes += isOpen ? " open" : ""
+
+    return (<div className={classes}>
+        <h2>Congrats on scoring the game!</h2>
+        <Button handleClick={() => {handleStart()}} text="Start Over" />
+    </div>)
+}
+
+export default Modal
